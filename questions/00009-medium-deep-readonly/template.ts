cabb34a8155ae666ffc6359c11f1d054fa7204ae
Seoin <seoin2269@naver.com>
2025-02-07 15:52:28 +0900

    refactor: 객체 타입 정의 Record로 수정

--- questions/00009-medium-deep-readonly/template.ts
+++ questions/00009-medium-deep-readonly/template.ts
@@ -1,7 +1,7 @@
 type DeepReadonly<T> =
   { readonly [K in keyof T]: T[K] extends (...args: any) => any
     ? T[K]
-    : T[K] extends { [k in string]: any }
+    : T[K] extends Record<any, any>
       ? DeepReadonly<T[K]>
       : T[K]
   }
